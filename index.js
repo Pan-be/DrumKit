6b67ec34f831469499a585784a5525b5804bc6b3
Konrad <konradbieniek@gmail.com>
2023-06-28 22:59:56 +0200

    added Audio object

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const btns = document.querySelectorAll(".drum")
 
 for (const btn of btns) {
-	btn.addEventListener("click", () => console.log(btn))
+	btn.addEventListener("click", () => {
+		const audio = new Audio("./sounds/tom-1.mp3")
+		audio.play()
+	})
 }
